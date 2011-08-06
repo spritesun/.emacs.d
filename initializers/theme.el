74df52de21228e6aab8c33f56156d26c080c1498
Long Sun <sun.long@rea-group.com>
2011-08-06 16:25:13 +1000

    change theme to the more terminal-like one

--- initializers/theme.el
+++ initializers/theme.el
@@ -1,4 +1,9 @@
-(require 'color-theme)
-(color-theme-gnome2)
+;;green theme
+;;(require 'color-theme)
+;;(color-theme-gnome2)
+
+;;dark theme
+(require 'color-theme-gruber-darker)
+(color-theme-gruber-darker)
 
 (set-face-attribute 'default nil :height 140)
