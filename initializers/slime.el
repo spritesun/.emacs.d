65996975f5c54251c7690a07646ac95fe11469f6
Long Sun <sun.long@rea-group.com>
2011-08-06 16:24:49 +1000

    clear the slime loading, it works now

--- initializers/slime.el
+++ initializers/slime.el
@@ -1,22 +1,7 @@
 (setq inferior-lisp-program "/opt/local/bin/sbcl")
 (add-to-list 'load-path "~/.emacs.d/plugins/slime/")
 (require 'slime)
-(slime-setup)
- ;; '(inferior-slime
- ;;   slime-asdf
- ;;   slime-autodoc
- ;;   slime-banner
- ;;   slime-c-p-c
- ;;   slime-editing-commands
- ;;   slime-fancy-inspector
- ;;   slime-fancy
- ;;   slime-fuzzy
- ;;   slime-highlight-edits
- ;;   slime-parse
- ;;   slime-presentation-streams
- ;;   slime-presentations
- ;;   slime-references
- ;;   slime-scratch
- ;;   slime-tramp
- ;;   slime-typeout-frame
- ;;   slime-xref-browser))
+;; (slime-setup)
+(slime-setup '(slime-fancy))  ; load almost all slime
+;; (require 'slime-autoloads) ; I guess it auoload slime mode to lisp file
+
